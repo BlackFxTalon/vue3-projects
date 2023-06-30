42d5ce2b55cec95f03d902883de32ea3b42cf5c7
NinjiClaw <andyv@inbox.ru>
2023-06-30 14:51:54 +0500

    done pokemon app

---
+++ pokemon-app/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="p-14">
+      <nav>
+        <RouterLink to="/" class="flex justify-center text-4xl text-yellow-700">Pokemon Picker</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
